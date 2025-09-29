8472dd451e4ed04b48bdc92efe3f2a1ac1b640c4
Francisco Ramos <framos@stack-ai.com>
2025-09-28 18:49:46 -0700

    create repo files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use aigenda::app::App;
+use aigenda::cli::Cli;
+use aigenda::error::Result;
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let app = App::new()?;
+    app.run(cli)
 }
